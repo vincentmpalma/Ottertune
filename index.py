4ad153842d12a56d1037e6ac6b133871d3303648
Janniel Tan <tanjanniel@gmail.com>
2025-04-23 14:36:02 -0700

    Like songs impelementation, search history on comment rn

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap5
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -28,9 +28,43 @@ app = Flask(__name__)
 boostrap = Bootstrap5(app)
 
 
+
+# liking the song
+cur.execute("""CREATE TABLE IF NOT EXISTS likedSongs(
+            likeID INTEGER PRIMARY KEY AUTOINCREMENT,
+            userId INTEGER,
+            songID INTEGER,
+            songURL TEXT,
+            FOREIGN KEY (userId) REFERENCES user(userId)
+            )""")
+con.commit() 
+
+#search history later
+# cur.execute("""CREATE TABLE IF NOT EXISTS searchHistory(
+#           searchID INTEGER PRIMARY KEY AUTOINCREMENT,
+#           userId INTEGER,
+#           songSearched TEXT,
+#           FOREIGN KEY (userId) REFERENCES user(userId)
+#           )""")
+# con.commit()
+
+
+                
+                
+
 @app.route('/')
 def index():
-    return  render_template('landing.html')
+
+    # will use later for search history
+    # userID = request.args.get('userId')
+    # searchHistory = []
+
+    # if userID:
+    #     cur.execute("SELECT songSearched FROM searchHistory WHERE userId = ? LIMIT 10", (userID))
+    #     searchHistory = cur.fetchall()
+
+    return  render_template('index.html')
+    #add it to render later searchHistory = searchHistory, userID = userID
 
 @app.route('/signIn', methods=['GET', 'POST'])
 def signIn():
@@ -57,9 +91,19 @@ def signUp():
 @app.route('/searchResults')
 def searchResults():
     track = request.args.get('track')
+
+    # will use later for the search history
+    # userID = request.args.get('userId')
+    
+    # if track and userID: #WORK IN PROGRESS
+    #     cur.execute("INSERT INTO searchHistory (userId, songSearched) VALUES (?,?)", (userID, track))
+    #     con.commit()
+
     results = sp.search(q=track, type='track', limit=5)
     results = results['tracks']['items']
-    return render_template('searchResults.html', results=results, track=track)
+    return render_template('searchResults.html', results=results, track_name=track)
+
+
 
 @app.route('/songInfo/<track_id>')
 def song_info(track_id):
@@ -72,3 +116,37 @@ def song_info(track_id):
     
     return render_template("songInfo.html", track=track, artist_albums = artistAlbums['items'], artist_image = imageArtists)
 
+@app.route('/likedSongs', methods =['POST'])
+def likedSongs():
+    userID = request.form['userId']
+    songID = request.form['songID']
+    songURL = request.form['songURL']
+    print(f"userID: {userID}")
+    print(f"songID: {songID}")
+    print(f"songURL: {songURL}")
+   
+
+    # for database purposes so there's no duplicates
+    cur.execute("SELECT * FROM likedSongs WHERE userId = ? AND songID = ? AND songURL = ?", (userID, songID, songURL))
+    already_liked = cur.fetchone()
+
+    if not already_liked:
+        cur.execute("INSERT INTO likedSongs (userId, songID, songURL) VALUES (?,?,?)", (userID, songID, songURL))
+        con.commit()
+
+    #searched song
+    track_query = request.form['trackQuery']
+    return redirect(url_for('searchResults', track=track_query, userId=userID, liked=songID, message="Added to Liked Songs"))
+
+
+#i need to figure out the user thing. Because right now im just liking the song but im not signed in yet
+@app.route('/checkLikes')
+def checkLikes():
+    userID = request.args.get('userId')
+    likedSongs = []
+
+    if userID:
+        cur.execute("SELECT songID, songURL FROM likedSongs WHERE userId = ?", (userID))
+        likedSongs = cur.fetchall()
+
+    return render_template('likedSongs.html', likedSongs =likedSongs, userID = userID)
